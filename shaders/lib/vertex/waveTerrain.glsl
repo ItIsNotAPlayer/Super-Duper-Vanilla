c3b730247c139215225102d6be0fa700c8234c5b
Eldeston <fremfredric@gmail.com>
2025-06-12 17:08:49 +0400

    Formatting

--- shaders/lib/vertex/waveTerrain.glsl
+++ shaders/lib/vertex/waveTerrain.glsl
@@ -1,55 +1,55 @@
 // Wave animation movements for shadow
-vec3 getTerrainWave(in vec3 vertexShdEyePlayerPos, in vec2 vertexShadowWorldPosXZ, in float midBlockY, in float id, in float outside, in float currTime){
+vec3 getTerrainWave(in vec3 vertexEyePlayerPos, in vec2 vertexWorldPosXZ, in float midBlockY, in float id, in float outside, in float currTime){
     // Wind affected blocks
     if(WIND_SPEED > 0){
         // Calculate wind strength
-        float windStrength = sin(-sumOf(id == 10801 ? floor(vertexShadowWorldPosXZ) : vertexShadowWorldPosXZ) * WIND_FREQUENCY + currTime * WIND_SPEED) * outside;
+        float windStrength = sin(-sumOf(id == 10801 ? floor(vertexWorldPosXZ) : vertexWorldPosXZ) * WIND_FREQUENCY + currTime * WIND_SPEED) * outside;
 
         // Simple blocks, horizontal movement
         if(id >= 10000 && id <= 10499){
-            vertexShdEyePlayerPos.xz += windStrength * 0.1;
-            return vertexShdEyePlayerPos;
+            vertexEyePlayerPos.xz += windStrength * 0.1;
+            return vertexEyePlayerPos;
         }
 
         // Single and double grounded cutouts
         if(id >= 10600 && id <= 10700){
             float isUpper = id == 10700 ? midBlockY - 1.5 : midBlockY - 0.5;
-            vertexShdEyePlayerPos.xz += isUpper * windStrength * 0.125;
-            return vertexShdEyePlayerPos;
+            vertexEyePlayerPos.xz += isUpper * windStrength * 0.125;
+            return vertexEyePlayerPos;
         }
 
         // Single hanging cutouts
         if(id == 10800 || id == 10801){
             float isLower = midBlockY + 0.5;
-            vertexShdEyePlayerPos.xz += isLower * windStrength * 0.0625;
-            return vertexShdEyePlayerPos;
+            vertexEyePlayerPos.xz += isLower * windStrength * 0.0625;
+            return vertexEyePlayerPos;
         }
 
         // Multi wall cutouts
         if(id == 10900){
-            vertexShdEyePlayerPos.xz += windStrength * 0.05;
-            return vertexShdEyePlayerPos;
+            vertexEyePlayerPos.xz += windStrength * 0.05;
+            return vertexEyePlayerPos;
         }
     }
 
     // Current affected blocks
     if(CURRENT_SPEED > 0){
         // Calculate current strength
-        float currentStrength = cos(-sumOf(vertexShadowWorldPosXZ) * CURRENT_FREQUENCY + currTime * CURRENT_SPEED);
+        float currentStrength = cos(-sumOf(vertexWorldPosXZ) * CURRENT_FREQUENCY + currTime * CURRENT_SPEED);
 
         // Simple blocks, vertical movement
         if(id == 11100 || id == 11101){
-            vertexShdEyePlayerPos.y += currentStrength * 0.0625;
-            return vertexShdEyePlayerPos;
+            vertexEyePlayerPos.y += currentStrength * 0.0625;
+            return vertexEyePlayerPos;
         }
 
         // Single and double grounded cutouts
         if(id == 11600){
             float isUpper = midBlockY - 0.5;
-            vertexShdEyePlayerPos.xz += isUpper * currentStrength * 0.125;
-            return vertexShdEyePlayerPos;
+            vertexEyePlayerPos.xz += isUpper * currentStrength * 0.125;
+            return vertexEyePlayerPos;
         }
     }
 
-    return vertexShdEyePlayerPos;
+    return vertexEyePlayerPos;
 }
--- shaders/lib/vertex/waveWater.glsl
+++ shaders/lib/vertex/waveWater.glsl
@@ -1,25 +1,17 @@
 // Wave animation movements for shadow
-vec3 getWaterWave(in vec3 vertexShdEyePlayerPos, in vec2 vertexShadowWorldPosXZ, in float id, in float currTime){
+vec3 getWaterWave(in vec3 vertexEyePlayerPos, in vec2 vertexWorldPosXZ, in float id, in float currTime){
     // Current affected blocks
     if(CURRENT_SPEED > 0){
         // Calculate current strength
-        float currentStrength = cos(-sumOf(vertexShadowWorldPosXZ) * CURRENT_FREQUENCY + currTime * CURRENT_SPEED);
+        float currentStrength = cos(-sumOf(vertexWorldPosXZ) * CURRENT_FREQUENCY + currTime * CURRENT_SPEED);
 
         // Water
         if(id == 11102){
-            #ifdef PHYSICS_OCEAN
-                // basic texture to determine how shallow/far away from the shore the water is
-                float physics_localWaviness = texelFetch(physics_waviness, ivec2(gl_Vertex.xz) - physics_textureOffset, 0).r;
+            vertexEyePlayerPos.y += currentStrength * 0.0625;
 
-                // transform gl_Vertex (since it is the raw mesh, i.e. not transformed yet)
-                vertexShdEyePlayerPos.y += physics_waveHeight((gl_Vertex.xz - physics_waveOffset) * PHYSICS_XZ_SCALE * physics_oceanWaveHorizontalScale, physics_localWaviness);
-            #endif
-
-            vertexShdEyePlayerPos.y += currentStrength * 0.0625;
-
-            return vertexShdEyePlayerPos;
+            return vertexEyePlayerPos;
         }
     }
 
-    return vertexShdEyePlayerPos;
+    return vertexEyePlayerPos;
 }
