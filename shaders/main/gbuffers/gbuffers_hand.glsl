f7b87a2eb8d691ff37784c83aff5c864b8771b04
Eldeston <fremfredric@gmail.com>
2025-06-24 10:15:38 +0400

    Fixed player hand translucents

--- shaders/main/gbuffers/gbuffers_hand.glsl
+++ shaders/main/gbuffers/gbuffers_hand.glsl
@@ -103,7 +103,7 @@
 
 #ifdef FRAGMENT
     /* RENDERTARGETS: 0,1,2,3 */
-    layout(location = 0) out vec3 sceneColOut; // gcolor
+    layout(location = 0) out vec4 sceneColOut; // gcolor
     layout(location = 1) out vec3 normalDataOut; // colortex1
     layout(location = 2) out vec3 albedoDataOut; // colortex2
     layout(location = 3) out vec3 materialDataOut; // colortex3
@@ -201,11 +201,11 @@
         material.albedo.rgb = toLinear(material.albedo.rgb);
 
         // Write to HDR scene color
-        sceneColOut = complexShadingForward(material);
+        sceneColOut = vec4(complexShadingForward(material), material.albedo.a);
 
         // Write buffer datas
         normalDataOut = material.normal;
         albedoDataOut = material.albedo.rgb;
-        materialDataOut = vec3(material.metallic, material.smoothness, 0);
+        materialDataOut = vec3(material.metallic, material.smoothness, 0.5);
     }
 #endif
